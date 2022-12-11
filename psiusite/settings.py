2f29a13e697335073bbdf9707f2d6d887170db7b
Mariana Mendes <mml0804@usp.br>
2022-12-11 19:29:17 -0300

    3.3.5 sem hexadecimal e logo.svg

--- psiusite/settings.py
+++ psiusite/settings.py
@@ -116,8 +116,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') # linha adicionada 3.3.5
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [ os.path.join(BASE_DIR, 'static'), ]  # linha adicionada 3.3.5
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
