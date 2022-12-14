ebccc2d0d602e6a27a3c3b3ed75a49de475040fe
isabela <isabelamesteves@usp.br>
2022-12-14 12:50:39 -0300

    4.5.4corrcted

--- esqueleto/models.py
+++ esqueleto/models.py
@@ -10,15 +10,17 @@ class Gostos(models.Model):
 
 
 class GostosDoPaciente(models.Model):
-    nome_do_paciente =  models.CharField(max_length=255)
+    nome_do_paciente =  models.ForeignKey(settings.AUTH_USER_MODEL,
+                               on_delete=models.CASCADE)
     gostos = models.ForeignKey(Gostos, on_delete=models.CASCADE)
 
-    #def __str__(self):
-       # return f'"{self.text}" - {self.author.username}'
+    def __str__(self):
+       return f'"{self.list(gostos)}" - {self.nome_do_paciente.username}'
 
 class GostosDoPsico(models.Model):
-    nome_do_psico =  models.CharField(max_length=255)
+    nome_do_psico =  models.ForeignKey(settings.AUTH_USER_MODEL,
+                               on_delete=models.CASCADE)
     gostos = models.ForeignKey(Gostos, on_delete=models.CASCADE)
 
-    #def __str__(self):
-        #return f'"{self.text}" - {self.author.username}'
+    def __str__(self):
+        return f'"{self.list(gostos)}" - {self.nome_do_psico.username}'
