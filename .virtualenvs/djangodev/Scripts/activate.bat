771b9bca1a834ede32f1340c86d97ed8dc7a766e
Mariana Mendes <mml0804@usp.br>
2023-02-07 21:58:05 -0300

    profile picture e bio

--- .virtualenvs/djangodev/Scripts/activate.bat
+++ .virtualenvs/djangodev/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\isabe\PsiuNovo\psiu\.virtualenvs\djangodev
+set VIRTUAL_ENV=C:\Users\Mariana\Documents\psiuagoravai\.virtualenvs\djangodev
 
 if not defined PROMPT set PROMPT=$P$G
 
